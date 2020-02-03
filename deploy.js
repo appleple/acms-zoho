27b47bef980eb51a9cbb5319c691bf66fdb046e5
Hori Godai <ess_president@me.com>
2020-02-03 11:17:31 +0900

    fix: http connector

--- deploy.js
+++ deploy.js
@@ -2,6 +2,7 @@ const cmd = require('node-cmd');
 const fs = require('fs-extra');
 const co = require('co');
 const archiver = require('archiver');
+
 const pkg = fs.readJsonSync('./package.json');
 
 /**
@@ -10,66 +11,63 @@ const pkg = fs.readJsonSync('./package.json');
  * @param cmdString
  * @returns {Promise}
  */
-const systemCmd = cmdString => {
-  return new Promise((resolve) => {
-    cmd.get(
-      cmdString,
-      (data, err, stderr) => {
-        console.log(cmdString);
-        console.log(data);
-        if (err) {
-          console.log(err);
-        }
-        if (stderr) {
-          console.log(stderr);
-        }
-        resolve(data);
+const systemCmd = cmdString => new Promise((resolve) => {
+  cmd.get(
+    cmdString,
+    (data, err, stderr) => {
+      console.log(cmdString);
+      console.log(data);
+      if (err) {
+        console.log(err);
       }
-    );
+      if (stderr) {
+        console.log(stderr);
+      }
+      resolve(data);
+    }
+  );
+});
+
+const zipPromise = (src, dist) => new Promise((resolve, reject) => {
+  const archive = archiver.create('zip', {});
+  const output = fs.createWriteStream(dist);
+
+  // listen for all archive data to be written
+  output.on('close', () => {
+    console.log(`${archive.pointer()} total bytes`);
+    console.log('Archiver has been finalized and the output file descriptor has closed.');
+    resolve();
   });
-}
 
-const zipPromise = (src, dist) => {
-  return new Promise((resolve, reject) => {
-    const archive = archiver.create('zip', {});
-    const output = fs.createWriteStream(dist);
-
-    // listen for all archive data to be written
-    output.on('close', () => {
-      console.log(archive.pointer() + ' total bytes');
-      console.log('Archiver has been finalized and the output file descriptor has closed.');
-      resolve();
-    });
-
-    // good practice to catch this error explicitly
-    archive.on('error', (err) => {
-      reject(err);
-    });
-
-    archive.pipe(output);
-    archive.directory(src).finalize();
+  // good practice to catch this error explicitly
+  archive.on('error', (err) => {
+    reject(err);
   });
-}
+
+  archive.pipe(output);
+  archive.directory(src).finalize();
+});
 
 co(function* () {
   try {
-    fs.mkdirsSync(`Zoho`);
-    fs.mkdirsSync(`build`);
-    fs.copySync(`./composer.json`, 'Zoho/composer.json');
-    fs.copySync(`./composer.lock`, `Zoho/composer.lock`);
-    fs.copySync(`./LICENSE`, `Zoho/LICENSE`);
-    fs.copySync(`./README.md`, `Zoho/README.md`);
-    fs.copySync(`./Engine.php`, `Zoho/Engine.php`);
-    fs.copySync(`./Api.php`, `Zoho/Api.php`);
-    fs.copySync(`./Hook.php`, `Zoho/Hook.php`);
+    fs.mkdirsSync('Zoho');
+    fs.mkdirsSync('build');
+    fs.copySync('./composer.json', 'Zoho/composer.json');
+    fs.copySync('./composer.lock', 'Zoho/composer.lock');
+    fs.copySync('./LICENSE', 'Zoho/LICENSE');
+    fs.copySync('./README.md', 'Zoho/README.md');
+    fs.copySync('./Engine.php', 'Zoho/Engine.php');
+    fs.copySync('./Api.php', 'Zoho/Api.php');
+    fs.copySync('./Hook.php', 'Zoho/Hook.php');
     fs.copySync('./theme', 'Zoho/theme');
-    fs.copySync(`./ServiceProvider.php`, `Zoho/ServiceProvider.php`);
-    fs.copySync(`./GET/Zoho/Admin.php`, `Zoho/GET/Zoho/Admin.php`);
-    fs.copySync(`./POST/Zoho/Deauthorize.php`, `Zoho/POST/Zoho/Deauthorize.php`);
-    fs.copySync(`./POST/Zoho/Oauth.php`, `Zoho/POST/Zoho/Oauth.php`);
-    yield systemCmd(`cd ./Zoho; composer install`);
-    yield zipPromise(`Zoho`, `./build/Zoho.zip`);
-    fs.removeSync(`Zoho`);
+    fs.copySync('./ServiceProvider.php', 'Zoho/ServiceProvider.php');
+    fs.copySync('./GET/Zoho/Admin.php', 'Zoho/GET/Zoho/Admin.php');
+    fs.copySync('./POST/Zoho/Deauthorize.php', 'Zoho/POST/Zoho/Deauthorize.php');
+    fs.copySync('./POST/Zoho/Oauth.php', 'Zoho/POST/Zoho/Oauth.php');
+    yield systemCmd('cd ./Zoho; composer install');
+    fs.copySync('./fix/ZohoHTTPConnector.php', 'Zoho/vendor/zohocrm/php-sdk/src/com/zoho/crm/library/common/ZohoHTTPConnector.php');
+    yield zipPromise('Zoho', './build/Zoho.zip');
+    fs.removeSync('Zoho');
     yield systemCmd('git add -A');
     yield systemCmd(`git commit -m "v${pkg.version}"`);
     yield systemCmd('git push');
