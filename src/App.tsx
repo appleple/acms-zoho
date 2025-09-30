61fec2381c217379ae16dc6ddef62d4940ec6389
nagatomi <nagatomi.takayuki81@gmail.com>
2025-09-30 11:59:36 +0900

    フィールド設定のUIをselectに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { SWRConfig } from 'swr'
+import DispatchCacheClear from './dispatch/dispatch-cache-clear';
 import DispatchLinkField from './dispatch/dispatch-link-field';
 
 export default function App() {
-  return <DispatchLinkField />;
+  return (
+    <>
+      <SWRConfig value={{ provider: () => new Map() }}>
+        <DispatchCacheClear />
+        <DispatchLinkField />
+      </SWRConfig>
+    </>
+  );
 }
---
+++ src/features/link-field/link-field-row.tsx
@@ -0,0 +1,55 @@
+import { useState, CSSProperties, useEffect } from 'react';
+import { ModuleSelect } from './module-select';
+import { ModuleFieldSelect } from './module-field-select';
+import { parseApiNames } from '../../utils';
+
+interface Props {
+  moduleInputRef: HTMLInputElement;
+  moduleFieldInputRef: HTMLInputElement;
+  moduleName: string;
+  moduleValue: string;
+  moduleFieldName: string;
+  moduleFieldValue: string;
+}
+
+export const LinkFieldRow = ({
+  moduleInputRef,
+  moduleFieldInputRef,
+  moduleName,
+  moduleValue,
+  moduleFieldName,
+  moduleFieldValue,
+}: Props) => {
+  const [selectedModuleApiNames, setSelectedModuleApiNames] = useState<string[]>([]);
+
+  // moduleValueが変更された時に、selectedModuleApiNamesを更新
+  useEffect(() => {
+    if (moduleValue) {
+      const apiNames = parseApiNames(moduleValue);
+      setSelectedModuleApiNames(apiNames);
+    }
+  }, [moduleValue]);
+
+  return (
+    <div
+      className="acms-admin-cssgrid"
+      style={{ '--acms-admin-columns': 2 } as CSSProperties }
+    >
+      <ModuleSelect
+        name={moduleName}
+        defaultValue={moduleValue}
+        originalInputRef={moduleInputRef}
+        onChange={(values) => {
+          const apiNames = values.map(module => module.apiName);
+          setSelectedModuleApiNames(apiNames);
+        }}
+      />
+      <ModuleFieldSelect
+        name={moduleFieldName}
+        value={moduleFieldValue}
+        moduleApiNames={selectedModuleApiNames}
+        originalInputRef={moduleFieldInputRef}
+      />
+    </div>
+  );
+};
